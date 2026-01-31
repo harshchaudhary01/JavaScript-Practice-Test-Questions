16726d27175fb0db831e52898f49ae0aae18abed
harshchaudhary01 <harshcareer01@gmail.com>
2026-01-31 09:36:09 +0530

    Completing the 2nd  Practice Question

---
+++ Q2/script.js
@@ -0,0 +1,11 @@
+const heading = document.getElementById('heading');
+heading.innerText = 'Welcome to JavaScript DOM Manipulation!';
+heading.style.color = 'blue';
+heading.style.backgroundColor = 'white';
+
+heading.style.display = 'flex';
+heading.style.justifyContent = 'center';
+heading.style.alignItems = 'center';
+heading.style.height = '100vh';
+
+heading.classList.add('main-heading');
