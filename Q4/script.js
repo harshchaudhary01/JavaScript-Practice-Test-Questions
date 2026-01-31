9f497fe308bbd62fb893df99950f6cae9b3050ca
harshchaudhary01 <harshcareer01@gmail.com>
2026-01-31 09:52:40 +0530

    Completing the 4th  Practice Question

---
+++ Q4/script.js
@@ -0,0 +1,11 @@
+const shoppingCart = [
+  { itemName: "Wireless Mouse", itemPrice: 25 },
+  { itemName: "Gaming Monitor", itemPrice: 300 },
+  { itemName: "USB-C Cable", itemPrice: 15 },
+  { itemName: "Mechanical Keyboard", itemPrice: 120 },
+  { itemName: "Webcam", itemPrice: 85 }
+];
+
+const newCart = shoppingCart.slice().sort((a, b) => a.itemPrice - b.itemPrice);
+
+console.log(newCart);
